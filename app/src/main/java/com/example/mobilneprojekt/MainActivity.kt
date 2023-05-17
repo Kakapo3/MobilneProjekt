d0de6b1c4cb737d639456174425cfb14aac54315
KacperMajkowski <kacper20015@o2.pl>
2023-05-17 19:46:04 +0200

    I tried main menu in Jetpack format

--- app/src/main/java/com/example/mobilneprojekt/MainActivity.kt
+++ app/src/main/java/com/example/mobilneprojekt/MainActivity.kt
@@ -1,22 +1,111 @@
 package com.example.mobilneprojekt
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFontFamilyResolver
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mobilneprojekt.ui.theme.MobilneProjektTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            MobilneProjektTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.spacedBy(40.dp)) {
 
-        var gamesList = ArrayList<String>()
-        gamesList.add("Game 1")
-        gamesList.add("Game 2")
-        gamesList.add("Game 3")
+                            MakeTitle(
+                                title = "App title",
+                                modifier = Modifier
+                            )
 
-        val recycler = findViewById<RecyclerView>(R.id.eventsList)
-        recycler.layoutManager = LinearLayoutManager(this)
-        recycler.adapter = CustomAdapter(gamesList)
+                            LazyColumn(
+                                verticalArrangement = Arrangement.spacedBy(20.dp)
+                            ) {
+                                item {
+                                    MakeGameRow(title = "Game 1")
+                                    MakeGameRow(title = "Game 2")
+                                    MakeGameRow(title = "Game 3")
+                                }
+                            }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun MakeTitle(title: String, modifier: Modifier) {
+    Text(
+        text = title,
+        textAlign = TextAlign.Center,
+        fontSize = 60.sp
+    )
+}
+
+@Composable
+fun MakeGameRow(title: String) {
+    Row(horizontalArrangement = Arrangement.spacedBy(20.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.game_icon_temp),
+            contentDescription = "Temp icon - change it when you deploy a game",
+            Modifier.size(50.dp)
+        )
+
+        Text(
+            text = title,
+            textAlign = TextAlign.Center,
+            fontSize = 40.sp
+        )
+
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Play!")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MobilneProjektTheme {
+        Greeting("Android")
     }
 }
