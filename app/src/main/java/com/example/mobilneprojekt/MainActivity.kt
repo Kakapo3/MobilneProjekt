b9358cdfd2e377b07ee3fe192a36781a8eb7f266
KacperMajkowski <kacper20015@o2.pl>
2023-05-18 23:19:36 +0200

    Restructured Main Menu with Scaffold, implemented Horizontal Pager

--- app/src/main/java/com/example/mobilneprojekt/MainActivity.kt
+++ app/src/main/java/com/example/mobilneprojekt/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mobilneprojekt
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,13 +12,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFontFamilyResolver
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -27,7 +37,15 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.*
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.TopAppBarColors
 import com.example.mobilneprojekt.ui.theme.MobilneProjektTheme
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import kotlin.system.exitProcess
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,29 +53,33 @@ class MainActivity : ComponentActivity() {
         setContent {
             MobilneProjektTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.spacedBy(40.dp)) {
+                MainMenuScaffold()
+            }
+        }
+    }
+}
 
-                            MakeTitle(
-                                title = "App title",
-                                modifier = Modifier
-                            )
+@Composable
+fun OldMenu() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(40.dp)) {
 
-                            LazyColumn(
-                                verticalArrangement = Arrangement.spacedBy(20.dp)
-                            ) {
-                                item {
-                                    MakeGameRow(title = "Game 1")
-                                    MakeGameRow(title = "Game 2")
-                                    MakeGameRow(title = "Game 3")
-                                }
-                            }
-                    }
+            MakeTitle(
+                title = "App title",
+            )
+
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(20.dp)
+            ) {
+                item {
+                    MakeGameRow(title = "Game 1")
+                    MakeGameRow(title = "Game 2")
+                    MakeGameRow(title = "Game 3")
                 }
             }
         }
@@ -65,19 +87,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Composable
-fun MakeTitle(title: String, modifier: Modifier) {
+fun MakeTitle(title: String) {
     Text(
         text = title,
         textAlign = TextAlign.Center,
-        fontSize = 60.sp
+        fontSize = 50.sp
     )
 }
 
@@ -102,10 +116,54 @@ fun MakeGameRow(title: String) {
     }
 }
 
+@Composable
+fun MakeGameColumn(imgSrc : Int, title: String) {
+    Column() {
+        Image(
+            painter = painterResource(id = R.drawable.game_icon_temp),
+            contentDescription = "Temp icon - change it when you deploy a game",
+            Modifier.size(250.dp)
+        )
+
+        Text(
+            text = title,
+            textAlign = TextAlign.Center,
+            fontSize = 60.sp
+        )
+    }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+@ExperimentalFoundationApi
+fun GameScroll() {
+    HorizontalPager(count = 3) { page ->
+        MakeGameColumn(imgSrc = R.drawable.game_icon_temp, title = "Game $page")
+    }
+}
+
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
+@Composable
+fun MainMenuScaffold () {
+    Scaffold(
+        topBar = { TopAppBar(
+            title = { MakeTitle(title = "App Title") }) },
+
+        content = { GameScroll() },
+
+        bottomBar = { BottomAppBar(
+            content = { Button(onClick = { exitProcess(0) }) {
+                Text(text = "Exit", fontSize = 20.sp)
+            } })}
+    )
+}
+
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MobilneProjektTheme {
-        Greeting("Android")
-    }
+    MainMenuScaffold()
 }
