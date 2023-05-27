1701b3cd0dfbd1507166465077afc14ab9764218
KacperMajkowski <kacper20015@o2.pl>
2023-05-27 13:00:57 +0200

    Add minesweeper

--- app/src/main/java/com/example/mobilneprojekt/minesweeper/Field.kt
+++ app/src/main/java/com/example/mobilneprojekt/minesweeper/Field.kt
@@ -1,4 +1,19 @@
-package com.example.mobilneprojekt.minesweeper
+package com.example.minesweeper
 
-class Field {
+import android.content.Context
+import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatButton
+
+class Field(context: Context) : AppCompatButton(context) {
+
+    var hasBomb = false
+    var blank = false
+
+    fun hasBomb() : Int {
+        if (hasBomb) {
+            return 1
+        } else {
+            return 0
+        }
+    }
 }
